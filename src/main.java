e3695279667eda1646303ee13479ae76f987ee8f
程佳辉 <chengjiahui@chengjiahuis-MacBook-Air.local>
2022-08-12 21:10:07 +0800

    first commit

---
+++ src/main.java
@@ -0,0 +1,11 @@
+/**
+ * @Description TODO
+ * @Auther chengjiahui
+ * @Date 2022/8/12 21:08
+ * @Version 1.0
+ */
+public class main {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
